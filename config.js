d4801aaf1b3ac3ef1bd31c5ef7c6a5036a9ab234
Boris Kaul <localvoid@gmail.com>
2015-03-29 11:24:37 +0600

    publish: Initial commit
    generated from commit 5785d17b57fe86dbded43266db00435913b0a16a

---
+++ config.js
@@ -0,0 +1,30 @@
+benchmarkConfig({
+  "tests": "http://vdom-benchmark.github.io/vdom-benchmark/tests.js",
+  "contestants": [
+    {
+      "name": "uix [Dart]",
+      "url": "https://github.com/localvoid/uix",
+      "benchmarkUrl": "http://localhost:8080/"
+    },
+    {
+      "name": "VDom [Dart]",
+      "url": "http://github.com/localvoid/vdom",
+      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-vdom/"
+    },
+    {
+      "name": "React",
+      "url": "http://facebook.github.io/react/",
+      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-react/"
+    },
+    {
+      "name": "virtual-dom",
+      "url": "https://github.com/Matt-Esch/virtual-dom",
+      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-virtual-dom/"
+    },
+    {
+      "name": "Bobril",
+      "url": "https://github.com/Bobris/Bobril",
+      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-bobril/"
+    }
+  ]
+});
